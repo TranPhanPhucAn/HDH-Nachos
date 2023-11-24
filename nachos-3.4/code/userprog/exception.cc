aaac8fad45a780259b18e6ec0e2294c78cd3b9ae
TranPhanPhucAn <21120407@student.hcmus.edu.vn>
2023-11-24 10:35:15 +0700

    second commit

--- nachos-3.4/code/userprog/exception.cc
+++ nachos-3.4/code/userprog/exception.cc
@@ -48,16 +48,161 @@
 //	are in machine.h.
 //----------------------------------------------------------------------
 
+//Change program counter of system after 4 byte to continue load next calls.
+void IncreasePC(){
+    int counter = machine->ReadRegister(PCReg)
+    machine->WriteRegister(PrevPCReg, counter)
+    counter = machine->ReadRegister(NextPCReg)
+    machine->WriteRegister(PCReg,counter)
+    machine->WriteRegister(NextPCReg,counter + 4)
+}
+
+// Input: - User space address (int) 
+// - Limit of buffer (int) 
+// Output:- Buffer (char*) 
+// Purpose: Copy buffer from User memory space to System memory space 
+char* User2System(int virtAddr,int limit) 
+{ 
+    int i;// index 
+    int oneChar; 
+    char* kernelBuf = NULL; 
+    kernelBuf = new char[limit +1];//need for terminal string 
+    if (kernelBuf == NULL) 
+        return kernelBuf; 
+    memset(kernelBuf,0,limit+1); 
+    //printf("\n Filename u2s:"); 
+    for (i = 0 ; i < limit ;i++) 
+    { 
+        machine->ReadMem(virtAddr+i,1,&oneChar); 
+        kernelBuf[i] = (char)oneChar; 
+        //printf("%c",kernelBuf[i]); 
+        if (oneChar == 0) 
+            break; 
+    } 
+    return kernelBuf; 
+}
+
+// Input: - User space address (int) 
+// - Limit of buffer (int) 
+// - Buffer (char[]) 
+// Output:- Number of bytes copied (int) 
+// Purpose: Copy buffer from System memory space to User memory space 
+int System2User(int virtAddr,int len,char* buffer) 
+{ 
+    if (len < 0) return -1; 
+    if (len == 0)return len; 
+    int i = 0; 
+    int oneChar = 0 ; 
+    do{ 
+        oneChar= (int) buffer[i]; 
+        machine->WriteMem(virtAddr+i,1,oneChar); 
+        i ++; 
+    }while(i < len && oneChar != 0); 
+        return i; 
+} 
+
+
+
 void
 ExceptionHandler(ExceptionType which)
 {
     int type = machine->ReadRegister(2);
 
-    if ((which == SyscallException) && (type == SC_Halt)) {
-	DEBUG('a', "Shutdown, initiated by user program.\n");
-   	interrupt->Halt();
-    } else {
-	printf("Unexpected user mode exception %d %d\n", which, type);
-	ASSERT(FALSE);
+    switch(which){
+        case NoException: 
+            return; 
+        case SyscallException: 
+            switch (type){ 
+                case SC_Halt: 
+                    DEBUG(‘a’, "\n Shutdown, initiated by user program."); 
+                    printf ("\n\n Shutdown, initiated by user program."); 
+                    interrupt->Halt(); 
+                    break;
+                case SC_Create:
+                    {
+                        int virtAddr; 
+                        char* filename; 
+                        DEBUG(‘a’,"\n SC_Create call ..."); 
+                        DEBUG(‘a’,"\n Reading virtual address of filename"); 
+                        // Lấy tham số tên tập tin từ thanh ghi r4 
+                        virtAddr = machine->ReadRegister(4); 
+                        DEBUG (‘a’,"\n Reading filename."); 
+                        // MaxFileLength là = 32 
+                        filename = User2System(virtAddr,MaxFileLength+1); 
+                        if (filename == NULL) 
+                        { 
+                            printf("\n Not enough memory in system"); 
+                            DEBUG(‘a’,"\n Not enough memory in system"); 
+                            machine->WriteRegister(2,-1); // trả về lỗi cho chương 
+                            // trình người dùng 
+                            delete filename; 
+                            return;
+                        }
+                        } 
+                        DEBUG(‘a’,"\n Finish reading filename."); 
+                        //DEBUG(‘a’,"\n File name : '"<<filename<<"'"); 
+                        // Create file with size = 0 
+                        // Dùng đối tượng fileSystem của lớp OpenFile để tạo file, 
+                        // việc tạo file này là sử dụng các thủ tục tạo file của hệ điều 
+                        // hành Linux, chúng ta không quản ly trực tiếp các block trên 
+                        // đĩa cứng cấp phát cho file, việc quản ly các block của file 
+                        // trên ổ đĩa là một đồ án khác 
+                        if (!fileSystem->Create(filename,0))
+                        { 
+                            printf("\n Error create file '%s'",filename); 
+                            machine->WriteRegister(2,-1); 
+                            delete filename; 
+                            return; 
+                        } 
+                        machine->WriteRegister(2,0); // trả về cho chương trình 
+                        // người dùng thành công 
+                        delete filename; 
+                        break;
+                
+            }
+            default: 
+                printf("\n Unexpected user mode exception (%d %d)", which, 
+                type); 
+                interrupt->Halt();
+    
+        case PageFaultException:
+            DEBUG(‘a’, "\n Page Fault Exception."); 
+            printf ("\n\n Page Fault Exception."); 
+            interrupt->Halt(); 
+            break; 
+        case ReadOnlyException:
+            DEBUG(‘a’, "\n Read Only Exception."); 
+            printf ("\n\n Read Only Exception."); 
+            interrupt->Halt(); 
+            break; 
+        case BusErrorException:
+            DEBUG(‘a’, "\n Bus Error Exception."); 
+            printf ("\n\n Bus Error Exception."); 
+            interrupt->Halt(); 
+            break;
+        case AddressErrorException:
+            DEBUG(‘a’, "\n Address Error Exception."); 
+            printf ("\n\n Address Error Exception."); 
+            interrupt->Halt(); 
+            break;
+        case OverflowException:
+            DEBUG(‘a’, "\n Overflow Exception."); 
+            printf ("\n\n Overflow Exception."); 
+            interrupt->Halt(); 
+            break;
+        case IllegalInstrException:
+            DEBUG(‘a’, "\n Illegal Instruction Exception."); 
+            printf ("\n\n Illegal Instruction Exception."); 
+            interrupt->Halt(); 
+            break;
+        
     }
+
+    // if ((which == SyscallException) && (type == SC_Halt)) {
+	// DEBUG('a', "Shutdown, initiated by user program.\n");
+   	// interrupt->Halt();
+    // } else {
+	// printf("Unexpected user mode exception %d %d\n", which, type);
+	// ASSERT(FALSE);
+    // }
 }
