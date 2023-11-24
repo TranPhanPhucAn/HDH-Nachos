598ec61c3b8de951847cea92b52abe481163f091
TranPhanPhucAn <21120407@student.hcmus.edu.vn>
2023-11-24 11:19:08 +0700

    print,read char done

--- nachos-3.4/code/userprog/exception.cc
+++ nachos-3.4/code/userprog/exception.cc
@@ -105,19 +105,11 @@ void ExceptionHandlerReadChar() {
     char c = 0;
     gSynchConsole->Read(&c,1);
     machine->WriteRegister(2, (int)c);
-    int pcAfter = machine->ReadRegister(NextPCReg) + 4;
-    machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
-    machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
-    machine->WriteRegister(NextPCReg, pcAfter);
 }
 void ExceptionHandlerPrintChar() {
     char c = (char)machine->ReadRegister(4);
     gSynchConsole->Write(&c,1);
     machine->WriteRegister(2, 0);
-    int pcAfter = machine->ReadRegister(NextPCReg) + 4;
-    machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
-    machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
-    machine->WriteRegister(NextPCReg, pcAfter);
 }
 
 
@@ -173,7 +165,15 @@ ExceptionHandler(ExceptionType which)
                     printf ("\n\n Shutdown, initiated by user program."); 
                     interrupt->Halt(); 
                     break;
-                case SC_Create:
+                case SC_ReadChar:
+                    ExceptionHandlerReadChar();
+                    IncreasePC();
+                    break;
+                case SC_PrintChar:
+                    ExceptionHandlerPrintChar();
+                    IncreasePC();
+                    break;      
+                // case SC_Create:
                     {
                         int virtAddr; 
                         char* filename; 
